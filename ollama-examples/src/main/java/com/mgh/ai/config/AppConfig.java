d2b9b0b7dd0ab66fc1fbdb637f2b5745634c2dcc
manjugh <manjunath.gh@ondosystems.com>
2024-07-28 17:39:17 +0530

    Adding Embedding example using SimpeVectorStore

--- ollama-examples/src/main/java/com/mgh/ai/config/AppConfig.java
+++ ollama-examples/src/main/java/com/mgh/ai/config/AppConfig.java
@@ -1,14 +1,42 @@
 package com.mgh.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+
+import java.nio.charset.Charset;
 
 @SpringBootConfiguration
 public class AppConfig {
 
     @Bean
+    VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        return new SimpleVectorStore(embeddingModel);
+    }
+
+
+    @Value("classpath:organisation.txt")
+    private Resource resource;
+    @Bean
     ChatClient chatClient(ChatClient.Builder chatClientBuilder){
         return chatClientBuilder.build();
     }
+
+    @Bean
+    ApplicationRunner applicationRunner(VectorStore vectorStore) {
+        return args -> {
+            var textReader = new TextReader(resource);
+            textReader.setCharset(Charset.defaultCharset());
+            vectorStore.add(textReader.get());
+        };
+    }
+
 }
--- ollama-examples/src/main/java/com/mgh/ai/controller/OllammaEmbedController.java
+++ ollama-examples/src/main/java/com/mgh/ai/controller/OllammaEmbedController.java
@@ -1,4 +1,57 @@
 package com.mgh.ai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+
 public class OllammaEmbedController {
+
+
+    private final VectorStore vectorStore;
+
+    private final ChatClient chatClient;
+
+
+
+    public OllammaEmbedController(VectorStore vectorStore, ChatClient chatClient) {
+        this.vectorStore = vectorStore;
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/rag")
+    public String getEmbed(@RequestParam String query) {
+        SearchRequest searchRequest = SearchRequest.query(query).withTopK(1);
+        List<Document> documents = vectorStore.similaritySearch(searchRequest);
+        String releaventInfo = documents.stream().map(Document::getContent).collect(Collectors.joining(System.lineSeparator()));
+        var systemPrompt = """
+                You will be working as project guide. You will provide the requested information. Answer precisely to the question, do not add addition information.
+                Use the following information to answer the question:
+                {information}
+                """;
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemPrompt);
+        Message information = systemPromptTemplate.createMessage(Map.of("information", releaventInfo));
+
+        var userMessage = """
+                Tell me about the {query}
+                """;
+        PromptTemplate template = new PromptTemplate(userMessage);
+        UserMessage userMeesage1 = new UserMessage(template.create(Map.of("query", query)).getContents());
+        return chatClient.prompt(new Prompt(List.of(userMeesage1,information))).call().content();
+    }
 }
